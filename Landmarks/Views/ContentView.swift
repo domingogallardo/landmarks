12d6e5a773d4dfb7383660e4ef533d998c9d83e3
Domingo Gallardo <domingo.gallardo@ua.es>
2022-07-06 09:38:23 +0200

    Terminado paso 2 del tutorial

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Domingo Gallardo on 15/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
